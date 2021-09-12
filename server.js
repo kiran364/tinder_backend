ac4396b41accbed036aae7d0e83355ac3af57e4a
kiran364 <kumawatkiran364@gmail.com>
2021-09-12 14:31:53 +0530

    server.js file

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cardsRoute = require('./routes/cards');
+const cors = require('cors');
+const app = express();
+const PORT = 4000;
+
+
+app.use(express.json());
+app.use(cors());
+app.use("/cards", cardsRoute);
+
+
+mongoose.connect('mongodb+srv://root:kiran@cluster0.cjkd2.mongodb.net/tinderDB?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }).
+then(() => console.log('Database connected successfuly')).
+catch((err) => console.log(err));
+
+app.listen(PORT, () => console.log(`server listning at port no - ${PORT}`))
